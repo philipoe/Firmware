eaf0bc41073cac443503fc8b409665efbef7b728
Amir <amir.melzer@mavt.ethz.ch>
2015-01-22 13:37:40 +0100

    GPS:  Add height above the Ellipsoid info
    Modification of the GPS message, add height above the Ellipsoid info.

--- src/drivers/gps/ubx.cpp
+++ src/drivers/gps/ubx.cpp
@@ -722,7 +722,6 @@ UBX::payload_rx_done(void)
 		_gps_position->lat		= _buf.payload_rx_nav_pvt.lat;
 		_gps_position->lon		= _buf.payload_rx_nav_pvt.lon;
 		_gps_position->alt		= _buf.payload_rx_nav_pvt.hMSL;
-		_gps_position->alt_ellipsoid = _buf.payload_rx_nav_pvt.height;		/* Height above the Ellipsoid */
 
 		_gps_position->eph		= (float)_buf.payload_rx_nav_pvt.hAcc * 1e-3f;
 		_gps_position->epv		= (float)_buf.payload_rx_nav_pvt.vAcc * 1e-3f;
@@ -784,7 +783,7 @@ UBX::payload_rx_done(void)
 		_gps_position->alt	= _buf.payload_rx_nav_posllh.hMSL;
 		_gps_position->eph	= (float)_buf.payload_rx_nav_posllh.hAcc * 1e-3f; // from mm to m
 		_gps_position->epv	= (float)_buf.payload_rx_nav_posllh.vAcc * 1e-3f; // from mm to m
-
+		_gps_position->alt_ellipsoid = _buf.payload_rx_nav_posllh.height;
 		_gps_position->timestamp_position = hrt_absolute_time();
 
 		_rate_count_lat_lon++;
