507f29f96654178e58742c3f045189a7c1f58182
philipoe <philipp.oettershagen@mavt.ethz.ch>
2015-02-23 14:22:38 +0100

    sdlog2 logenabler: Improve RAM-consumption by storing handles only temporarily

--- src/modules/sdlog2/logenabler.h
+++ src/modules/sdlog2/logenabler.h
@@ -18,26 +18,28 @@ struct log_params {
 	bool LOG_ASLD;
 } pLogEnabler;
 
-struct log_param_handles {
-	param_t LOG_EKF;
-	param_t LOG_RCIn;
-	param_t LOG_MPPT;
-	param_t LOG_ASLC;
-	param_t LOG_ASLD;
-} hLogEnabler;
-
 void GetLogEnablerParams(void)
 {
-	hLogEnabler.LOG_EKF=param_find("LOG_EKF");
-	hLogEnabler.LOG_RCIn=param_find("LOG_RCIn");
-	hLogEnabler.LOG_MPPT=param_find("LOG_MPPT");
-	hLogEnabler.LOG_ASLC=param_find("LOG_ASLC");
-	hLogEnabler.LOG_ASLD=param_find("LOG_ASLD");
-
+	param_t handle;
 	int temp=0; // Need to convert here from int32 -> bool (bool is used to save RAM)
-	param_get(hLogEnabler.LOG_EKF, &(temp)); 	pLogEnabler.LOG_EKF=(temp==1 ? true:false);
-	param_get(hLogEnabler.LOG_RCIn, &(temp));	pLogEnabler.LOG_RCIn=(temp==1 ? true:false);
-	param_get(hLogEnabler.LOG_MPPT, &(temp)); 	pLogEnabler.LOG_MPPT=(temp==1 ? true:false);
-	param_get(hLogEnabler.LOG_ASLC, &(temp)); 	pLogEnabler.LOG_ASLC=(temp==1 ? true:false);
-	param_get(hLogEnabler.LOG_ASLD, &(temp)); 	pLogEnabler.LOG_ASLD=(temp==1 ? true:false);
+
+	handle = param_find("LOG_EKF");
+	param_get(handle, &(temp));
+	pLogEnabler.LOG_EKF=(temp==1 ? true:false);
+
+	handle = param_find("LOG_RCIn");
+	param_get(handle, &(temp));
+	pLogEnabler.LOG_RCIn=(temp==1 ? true:false);
+
+	handle = param_find("LOG_MPPT");
+	param_get(handle, &(temp));
+	pLogEnabler.LOG_MPPT=(temp==1 ? true:false);
+
+	handle = param_find("LOG_ASLC");
+	param_get(handle, &(temp));
+	pLogEnabler.LOG_ASLC=(temp==1 ? true:false);
+
+	handle = param_find("LOG_ASLD");
+	param_get(handle, &(temp));
+	pLogEnabler.LOG_ASLD=(temp==1 ? true:false);
 }
