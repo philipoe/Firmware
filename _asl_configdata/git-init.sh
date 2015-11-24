ec1af4d204339688a07d817ef85c20135c7d49be
philipoe <philipp.oettershagen@mavt.ethz.ch>
2015-11-24 15:43:02 +0100

    [asl_configdata] Had to disable the pre-comit git hook for now again, as it currently does not work with Git GUIs and there is no simple fix for this in sight.

--- _asl_configdata/git-init.sh
+++ _asl_configdata/git-init.sh
@@ -1,2 +1,6 @@
 #! /bin/bash -v
-cp _asl_configdata/asl_tools/hooks/pre-commit .git/hooks/
+
+# Update all ASL-specific initialisation activity here.
+
+# Pre-commit hook: Disabled at the moment, does not run with GIT GUIs
+#cp _asl_configdata/asl_tools/hooks/pre-commit .git/hooks/
